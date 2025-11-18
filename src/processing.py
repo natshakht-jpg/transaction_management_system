2187322766528d23e266b4757ab30addaf7219b8
Наталья Шахтарина <natshakht@yandex.ru>
2025-11-18 21:52:46 +0400

    Add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,16 @@ def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dic
         if operation['state'] == state:  # Если статус операции совпадает с искомым
             filtered_operations.append(operation)  # Добавляем операцию в результат
     return filtered_operations  # Возвращаем отфильтрованный список
+
+
+def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Сортирует список операций по дате.
+    Аргументы:
+        operations: Список словарей с операциями
+        reverse: Порядок сортировки (True - по убыванию, False - по возрастанию)
+    Возврат:
+        Отсортированный список операций
+    """
+    # Сортируем операции по дате с указанным порядком (по убыванию по умолчанию)
+    return sorted(operations, key=lambda x: x['date'], reverse=reverse)
